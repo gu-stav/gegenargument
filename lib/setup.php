53d1f6e6cad0374aab051def577c62886c2bd377
Gustav Pursche <pursche@posteo.de>
2016-05-26 12:39:36 +0200

    Cleanup empty lines in lib/setup.php

--- lib/setup.php
+++ lib/setup.php
@@ -57,5 +57,3 @@ function assets() {
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
-
-
