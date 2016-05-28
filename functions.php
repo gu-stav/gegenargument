a5bc77c492a18fd34f3d36b5f2a872729379d872
Gustav Pursche <pursche@posteo.de>
2016-05-28 14:35:13 +0200

    Fix typo when saving the fallback color

--- functions.php
+++ functions.php
@@ -55,7 +55,7 @@ function admin_save_post($post_id, $post) {
   if($has_parent) {
     $update = true;
   } else if(!$meta_color[$field_key][0]) {
-    $color = 'rgb(255, 255, 255, .25)';
+    $color = 'rgba(255, 255, 255, .25)';
     $update = true;
   }
 
